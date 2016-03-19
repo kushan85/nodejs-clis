9c9f57c5f371bfc84f08793e0b4b8606a3b6a589
Kushan Shah <KShah0@walmartlabs.com>
2016-03-19 16:19:05 -0700

    completed 6 and 7 from the assignment

--- rm.js
+++ rm.js
@@ -2,11 +2,27 @@
 'use strict'
 require('./helper')
 let fs = require('fs').promise
+let co = require('co')
+let path = require("path")
 
-function* rm() {
-    // Use 'yield' in here
-    // Your implementation here
-    
+let rm = co.wrap(function*(rootpath) {	
+	let filenames = yield fs.readdir(rootpath)
+	for(let file of filenames) {
+		let filepath = path.join(rootpath, file)
+		let stat = yield fs.stat(filepath)
+		if(stat.isDirectory()) {
+			yield rm(filepath)
+		} else {
+			console.log('Removed ' + filepath)
+			yield fs.unlink(filepath)
+		}
+	}
+	console.log('Removed ' + rootpath)
+	yield fs.rmdir(rootpath);
+})
+
+function* main() {
+	yield rm(process.argv[2])
 }
 
-module.exports = rm
+module.exports = main
